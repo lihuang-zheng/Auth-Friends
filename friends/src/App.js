9b5c987a1eb83d067d31beafa3a1682250fd8639
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-25 15:19:22 -0600

    removed manifest, and structured the base of app with routes.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// stlying for app
+import "./App.css";
+
+// import components
+import Login from "./Components/Login";
+import Friends from "./Components/Friends";
+
+// import private route and dependency
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route />
+        <PrivateRoute />
+        <Route />
+      </div>
+    </BrowserRouter>
   );
 }
 
