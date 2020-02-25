46df50c0b86ad80586b2f8465823f746cfb736e8
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-25 15:35:00 -0600

    structured the final app, now it usable.

--- friends/src/App.js
+++ friends/src/App.js
@@ -9,14 +9,15 @@ import Friends from "./Components/Friends";
 
 // import private route and dependency
 import { BrowserRouter, Route } from "react-router-dom";
+import PrivateRoute from "./Utils/axiosWithAuth";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Route />
-        <PrivateRoute />
-        <Route />
+        <Route path="/login" component={Login} />
+        <PrivateRoute exact path="/protected" component={Friends} />
+        <Route exact path="/" component={Login} />
       </div>
     </BrowserRouter>
   );
