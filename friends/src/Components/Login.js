a710e89b765724283aeec53a0cad9ca428f37d6f
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-25 17:09:39 -0600

    changed from window.localstorage to localstorage in order for it to update on the server.

--- friends/src/Components/Login.js
+++ friends/src/Components/Login.js
@@ -26,7 +26,7 @@ class Login extends React.Component {
     axiosWithAuth()
       .post("/api/login", this.state.credentials)
       .then(res => {
-        window.localStorage.setItem("token", res.data.payload);
+        localStorage.setItem("token", res.data.payload);
         this.props.history.push("/protected");
       })
       .catch(err => console.log(err.message));
--- friends/src/Components/PrivateRoute.js
+++ friends/src/Components/PrivateRoute.js
@@ -8,7 +8,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
     <Route
       {...rest}
       render={() => {
-        if (window.localStorage.getItem("token")) {
+        if (localStorage.getItem("token")) {
           return <Component />;
         } else {
           return <Redirect to="/login" />;
